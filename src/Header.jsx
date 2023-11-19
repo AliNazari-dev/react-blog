59db6ad87695c7da39fde4ed26e4d72af046bba8
Ali <AliNazarii7394@gmail.com>
2023-11-19 23:26:41 +0330

    refactor to useContext

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,14 @@
-import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa';
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
+import useWindows from "./hooks/useWindows";
 
-const Header = ({ title, width }) => {
-    return (
-        <header className="Header">
-            <h1>{title}</h1>
-            {width < 768 ? <FaMobileAlt />
-                : width < 992 ? <FaTabletAlt />
-                    : <FaLaptop />}
-        </header>
-    )
-}
+const Header = ({ title }) => {
+  const { width } = useWindows();
+  return (
+    <header className='Header'>
+      <h1>{title}</h1>
+      {width < 768 ? <FaMobileAlt /> : width < 992 ? <FaTabletAlt /> : <FaLaptop />}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
